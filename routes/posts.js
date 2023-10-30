569ebfdfeedda4af39eb1d221a67a6d4dc34e3ea
Aaron Saula <n00201123@iadt.ie>
2023-10-30 10:48:57 +0000

    Start

---
+++ routes/posts.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const { loginRequired } = require('../controllers/user.controller');
+
+const { readData,
+        readOne,
+        createData,
+        updateData,
+        deleteData} = require('../controllers/festival.controller');
+
+router.get('/', readData)
+      .get('/:id',loginRequired, readOne)
+      .post('/', loginRequired, createData)
+      .put('/:id', loginRequired, updateData)
+      .delete('/:id', loginRequired, deleteData);
+
+module.exports = router;
